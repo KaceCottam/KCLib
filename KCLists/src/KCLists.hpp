b6f0935991a937338915a6a7862fe91989435047
KaceCottam <kace.cottam@wsu.edu>
2019-02-15 13:08:54 -0800

    Added functions to KC::Lists Added TraversalNode class to simplify things.

--- KCLists/src/KCLists.hpp
+++ KCLists/src/KCLists.hpp
@@ -46,11 +46,12 @@ namespace KC
 			return *this;
 		}
 
-		static void LinkNodes(std::initializer_list<ListNode<T>> nodes)
+		static void LinkNodes(int const length, ListNode<T>* nodes)
 		{
-			ListNode<T>& trail = *nodes.begin();
-			for (auto& head : nodes)
+			ListNode<T>* trail = nodes;
+			for (auto i = 0; i < length; i++)
 			{
+				ListNode<T>* head = nodes + i;
 				if (trail != head)
 				{
 					trail->Next = head;
@@ -59,8 +60,57 @@ namespace KC
 				}
 			}
 		}
+		static void LinkNodes(std::initializer_list<ListNode<T>> nodes)
+		{
+			LinkNodes(nodes.size(), nodes.begin());
+		}
 
 		~ListNode() = default;
+	};
+
+	template <typename T>
+	class TraversalNode final
+	{
+	public:
+		ListNode<T>* Current;
+
+		TraversalNode(ListNode<T>* node) : Current(node)
+		{
+		}
+		TraversalNode<T>& operator++()
+		{
+			Current = Current->Next;
+			return *this;
+		}
+		TraversalNode<T>& operator--()
+		{
+			Current = Current->Previous;
+			return *this;
+		}
+		TraversalNode<T>& operator+=(int const num)
+		{
+			for (auto i = 0; i < num; i++)
+			{
+				*this++;
+			}
+			return *this;
+		}
+		TraversalNode<T>& operator-=(const int num)
+		{
+			for (auto i = 0; i < num; i++)
+			{
+				*this--;
+			}
+			return *this;
+		}
+		ListNode<T>& operator*()
+		{
+			return *Current;
+		}
+		ListNode<T>* operator->()
+		{
+			return Current;
+		}
 	};
 
 	template <class T>
@@ -86,9 +136,6 @@ namespace KC
 				Push(other.GetIndex(i));
 			}
 		}
-		//explicit List(T const& data) : Header{ new ListNode<T>(data) }, Length(1)
-		//{
-		//}
 		List(std::initializer_list<T> data)
 		{
 			Push(data);
@@ -113,14 +160,19 @@ namespace KC
 				throw std::out_of_range("Index is greater than the length of list!");
 			}
 
-			ListNode<T>* traversalNode = Header;
+			TraversalNode<T> traversalNode = Header;
 
-			for (auto i = 0; i < index; i++)
-			{
-				traversalNode = traversalNode->Next;
-			}
+			traversalNode += index;
 
 			return traversalNode->Data;
+		}
+
+		void Delete()
+		{
+			while (Header)
+			{
+				Pull();
+			}
 		}
 
 		void Push(T const& data)
@@ -129,8 +181,7 @@ namespace KC
 			Header = new ListNode<T>(data);
 			if (previousHeader)
 			{
-				Header->Next = previousHeader;
-				previousHeader->Previous = Header;
+				ListNode<T>::LinkNodes({ Header,previousHeader });
 			}
 			Length++;
 		}
@@ -145,6 +196,14 @@ namespace KC
 		{
 			auto length = data.size();
 			Push(length, data.begin());
+		}
+		void Push(List<T> const& other)
+		{
+			auto length = other.Length;
+			for (auto i = length; i > 0; --i)
+			{
+				Push(other.GetIndex(i));
+			}
 		}
 
 		T Pull()
@@ -166,21 +225,16 @@ namespace KC
 
 		List<T>& operator=(List<T> const& other)
 		{
-			while (Header)
-			{
-				Pull();
-			}
+			Delete();
 
-			auto length = other.Length;
-			for (auto i = length; i > 0; --i)
-			{
-				Push(other.GetIndex(i));
-			}
+			Push(other);
 
 			return *this;
 		}
 		List<T>& operator=(List<T>&& other) noexcept
 		{
+			Delete();
+
 			if (this != &other)
 			{
 				Header = other.Header;
@@ -213,10 +267,7 @@ namespace KC
 
 		~List()
 		{
-			while (Header)
-			{
-				Pull();
-			}
+			Delete();
 		}
 	};
 }
