9cbc5157edf0fd4a893877fc5004816cca1b4015
KaceCottam <kace.cottam@wsu.edu>
2019-02-25 18:00:17 -0800

    Test shtuff

---
+++ TestKCStateMachine/TestKCStateMachine.cpp
@@ -0,0 +1,10 @@
+#include <Test.h>
+#include <kcstatemachine.hpp>
+
+SCENARIO("A basic state machine with one data type")
+{
+	GIVEN("A state machine with an integer data type is initialized with an initializer_list")
+	{
+		auto how_many_increments = 0;
+	}
+}
