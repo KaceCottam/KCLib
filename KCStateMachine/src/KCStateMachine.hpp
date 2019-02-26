35aed959657bd0388f77c65e8bcd30d3df457422
KaceCottam <kace.cottam@wsu.edu>
2019-02-25 17:59:42 -0800

    Updated VS2017

--- KCStateMachine/src/KCStateMachine.hpp
+++ KCStateMachine/src/KCStateMachine.hpp
@@ -11,27 +11,27 @@ namespace KC
 	{
 		bool FlagStop = false;
 		int CurrentStateIndex = 0;
-		std::tuple<DataTypes> Data;
 	public:
+		std::tuple<DataTypes...> Data;
 		std::vector<std::function<void(StateMachine&)>> States;
 		std::function<void(StateMachine&)> NextState;
 
-		explicit StateMachine(DataTypes& data)
+		explicit StateMachine(DataTypes& ...data)
 			: Data(data)
 		{
 		}
 		explicit StateMachine(std::vector<std::function<void(StateMachine&)>> states)
-			: Data(nullptr), States(states), NextState(states.front())
+			: Data(), States(states), NextState(states.front())
 		{
 		}
 		explicit StateMachine(std::initializer_list<std::function<void(StateMachine&)>> states)
-			: Data(nullptr), States(std::vector<std::function<void(StateMachine&)>>(states)), NextState(states[0])
+			: Data(), States(states), NextState(States[0])
 		{
 		}
 
 		void Start()
 		{
-			while (NextState != nullptr && !FlagStop)
+			while (NextState != nullptr || !FlagStop)
 			{
 				NextState(*this);
 			}
@@ -50,19 +50,36 @@ namespace KC
 			return FlagStop;
 		}
 
-		StateMachine<DataTypes>& operator++()
+		StateMachine<DataTypes...>& operator++()
 		{
 			CurrentStateIndex++;
 			NextState = States[CurrentStateIndex];
 			return *this;
 		}
+		StateMachine<DataTypes...>& operator++(int)
+		{
+			auto temp(*this);
+			++*this;
+			return temp;
+		}
 
-		StateMachine<DataTypes>& operator--()
+		StateMachine<DataTypes...>& operator--()
 		{
 			CurrentStateIndex--;
 			while (CurrentStateIndex < 0) CurrentStateIndex++;
 			NextState = States[CurrentStateIndex];
 			return *this;
+		}
+		StateMachine<DataTypes...>& operator--(int)
+		{
+			auto temp(*this);
+			--*this;
+			return temp;
+		}
+
+		std::function<void(StateMachine&)>& operator[](int const index)
+		{
+			return States[index];
 		}
 
 		explicit operator bool() const { return IsStopped(); }
